18ad733de94b28a92e12941a10061c75ad41bbb5
dscreet <discreetbinger@gmail.com>
2024-06-11 14:14:26 +0100

    cleanup + error handling

--- app.py
+++ app.py
@@ -17,47 +17,40 @@ aka = {
     "xanax": "allergic to good botlanes"
 }
 
-def sort_leaderboard(data):
-    print('uh')
-    print(data)
+# simple yet effective way of ordering ranks of players
+def calc_player_rank_score(data):
     tiers = {'DIAMOND': 3, 'EMERALD': 2, 'PLATINUM': 1}
     ranks = {'IV': 1, 'III': 2, 'II': 3, 'I': 4}
     return (tiers.get(data['tier']) * 1000) + (ranks.get(data['rank']) * 100) + data['lp']
 
-
-def fetch_data():
-
+def get_stats():
     summoners = config['summoner_ids'].items()
-
-    processed_data = []
-    for k,v in summoners:
-        #error handling...
-        ranked_stats = lol_watcher.league.by_summoner("euw1", v)
-        soloq_stats = next(entry for entry in ranked_stats if entry['queueType'] == 'RANKED_SOLO_5x5')
-
-        games = soloq_stats["wins"] + soloq_stats["losses"]
-        winrate = round(soloq_stats["wins"] / games * 100)
-
-        processed_item = {
-            'name': k,
-            'aka': aka[k],
-            'tier': soloq_stats["tier"],
-            'rank': soloq_stats["rank"],
-            'lp': soloq_stats["leaguePoints"],
-            'disp': f'{soloq_stats["tier"]} {soloq_stats["rank"]} {soloq_stats["leaguePoints"]}lp',
-            'games': games,
-            'winrate': winrate
-        }
-
-        processed_data.append(processed_item)
-
-    return processed_data
+    all_player_stats = []
+    for name, summoner_id in summoners:
+        try:
+            # might contain both flex and soloq data
+            ranked_stats = lol_watcher.league.by_summoner("euw1", summoner_id)
+            soloq_stats = next(entry for entry in ranked_stats if entry['queueType'] == 'RANKED_SOLO_5x5')
+            games = soloq_stats["wins"] + soloq_stats["losses"]
+            winrate = round(soloq_stats["wins"] / games * 100)
+            player_stats = {
+                'name': name,
+                'aka': aka[name],
+                'tier': soloq_stats["tier"],
+                'rank': soloq_stats["rank"],
+                'lp': soloq_stats["leaguePoints"],
+                'full_rank': f'{soloq_stats["tier"]} {soloq_stats["rank"]} {soloq_stats["leaguePoints"]}lp',
+                'games': games,
+                'winrate': winrate
+            }
+            all_player_stats.append(player_stats)
+        except (ApiError, StopIteration) as e:
+            print(f'Error fetching data for summoner {name}: {e}')
+    return all_player_stats
     
-
-
 @app.route('/')
 def index():
-    sorted_leaderboard = sorted(fetch_data(), key=sort_leaderboard, reverse=True)
+    sorted_leaderboard = sorted(get_stats(), key=calc_player_rank_score, reverse=True)
     print(sorted_leaderboard)
     return render_template('index.html', data=sorted_leaderboard)
 
